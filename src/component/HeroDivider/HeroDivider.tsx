d09115b97fedb12a9aeb78b9cba8d86cbc482bdd
Savita Katwe <savitamerwade04@gmail.com>
2024-09-13 13:36:49 +0400

    divider

--- src/component/HeroDivider/HeroDivider.tsx
+++ src/component/HeroDivider/HeroDivider.tsx
@@ -1,23 +1,24 @@
 const HeroDivider = () => {
     return <>
-        <div className="bg-purple-primary">
-            <div>
+        <div
+            className="bg-purple-primary flex flex-col md:flex-row justify-between  items-center px-20 py-10 gap-8 md:px-36 md:py-10">
+            <div className="flex flex-col justify-center items-center gap-4">
                 <p className={"text-5xl text-white font-bold"}>10K+</p>
-                <p className={"text-sm text-purple-secondary"}>Satisfied Costumers
+                <p className={"flex text-sm text-purple-secondary items-center justify-center"}>Satisfied Costumers
                     All Great Over The World </p>
             </div>
-            <div>
-                <p className={"text-5xl text-white font-bold"}>10K+</p>
-                <p className={"text-sm text-purple-secondary"}>Satisfied Costumers
-                    All Great Over The World </p>
+
+            <div className="flex flex-col justify-center items-center gap-4">
+                <p className={"text-5xl text-white font-bold"}>4M</p>
+                <p className={"text-sm text-purple-secondary"}>Healthy Dishes Sold Including Milk Shakes Smooth </p>
             </div>
-            <div>
-                <p className={"text-5xl text-white font-bold"}>10K+</p>
-                <p className={"text-sm text-purple-secondary"}>Satisfied Costumers
-                    All Great Over The World </p>
+            <div className="flex flex-col justify-center items-center gap-4">
+                <p className={"text-5xl text-white font-bold"}>99.99%</p>
+                <p className={"text-sm text-purple-secondary"}>Reliable Customer Support We Provide Great
+                    Experiences </p>
             </div>
 
         </div>
     </>
 }
-export default HeroDivider
+export default HeroDivider;
