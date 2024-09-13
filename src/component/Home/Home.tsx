e1dcc8a845eb23a978bbfa44654165aa02147b38
Savita Katwe <savitamerwade04@gmail.com>
2024-09-13 16:22:35 +0400

    home section done for mobile

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -4,27 +4,31 @@ import {ArrowRight} from "lucide-react";
 
 const Home = () => {
     return <>
-        <div className={"flex flex-col md:flex-row justify-between md:m-12 "}>
-            <div className={"flex justify-center md:w-1/2 items-center"}>
+        <div className={"flex flex-col md:flex-row justify-between items-center md:m-12 "}>
+            <div className={"flex flex-1 order-2 md:order-1 md:w-1/2 items-center justify-center w-3/4 h-3/4"}>
                 <img src={MobileRestaurantPic} alt={"MobileRestaurantPic"}/>
             </div>
-            <div className={"w-3/4 md:w-1/2  m-5 md:m-12 justify-center "}>
-                <p className={"text-4xl md:text-7xl font-semibold md:pb-10"}>Premium <span
+            <div className={"md:w-1/2 m-5 md:m-12 flex-1 order-1 md:order-2"}>
+                <p className={"text-4xl md:text-7xl font-semibold pb-8 md:pb-10"}>Premium <span
                     className={"text-purple-primary"}>Quality </span><br/>For Your Health</p>
-                <ul className={"flex flex-col list-disc gap-4"}>
-                    <li className={"text-dark-tertiary"}>Premium quality food is made with ingredients that <br/> are
-                        packed with essential
-                        vitamins,
-                        minerals.
+                <ul className={"flex flex-col list-disc justify-center  md:justify-start gap-5"}>
+                    <li className={"text-dark-tertiary text-xs md:text-xl"}>Premium quality food is made
+                        with ingredients that <br/> are
+                        packed with essential vitamins, minerals.
                     </li>
-                    <li className={"text-dark-tertiary"}>These foods promote overall wellness by support<br/> healthy
+                    <li className={"text-dark-tertiary text-xs md:text-xl"}>These foods promote overall wellness by
+                        support<br/> healthy
                         digestion and boosting immunity
                     </li>
                 </ul>
-                <button
-                    className="p-5 md:mt-10 bg-purple-primary font-medium flex gap-2 flex-row border-solid border rounded-2xl text-white text-base">Download <ArrowRight/>
-                </button>
-                <img className={"md:ml-32"} src={CurlPointer} alt={CurlPointer}/>
+                <div className={"flex md:justify-start justify-center items-center"}>
+                    <button
+                        className="p-5 mt-5 md:mt-10 bg-purple-primary font-medium flex gap-2 flex-row border-solid border rounded-2xl text-white justify-center items-center">Download <ArrowRight/>
+                    </button>
+                </div>
+
+                <img className={"md:ml-32 md:rotate-0 rotate-90 mt-16 md:mt-0 mb-10 md:mb-0"} src={CurlPointer}
+                     alt={CurlPointer}/>
             </div>
 
         </div>
