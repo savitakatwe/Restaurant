2ac99e5b0658fc5db806f4450097d95d1334ad3e
Savita Katwe <savitamerwade04@gmail.com>
2024-09-13 18:49:10 +0400

    cards ready

--- src/component/RestaurantCard/RestaurantCard.tsx
+++ src/component/RestaurantCard/RestaurantCard.tsx
@@ -1,27 +1,39 @@
-import ChickenImage from "../../assets/ChickenImage.png"
 import StarIcon from "../../assets/StarIcon.svg"
 import BookMark from "../../assets/BookMark.svg"
+import {PropsWithChildren} from "react";
 
-const RestaurantCard = () => {
+interface RestaurantCardProps {
+    restaurantImage: string;
+    tagText: string;
+    restaurantName: string;
+    time: string;
+    rating: string;
+}
+
+const RestaurantCard = ({
+                            restaurantImage,
+                            tagText,
+                            restaurantName,
+                            time,
+                            rating
+                        }: PropsWithChildren<RestaurantCardProps>) => {
     return <>
         <div className={"h-fit w-fit border border-solid shadow-sm rounded-3xl overflow-hidden"}>
-            <img src={ChickenImage} alt="Chicken"/>
+            <img src={restaurantImage} alt="Chicken"/>
             <div className={"p-4"}>
                 <div className={"bg-yellow-secondary border rounded-md border-none w-fit px-2"}>
-                    <p className={'text-yellow-primary'}> Healthy </p></div>
-                <p className={" text-2xl font-semibold text-dark-black"}>The Chicken King</p>
+                    <p className={'text-yellow-primary'}> {tagText} </p></div>
+                <p className={" text-2xl font-semibold text-dark-black"}>{restaurantName}</p>
                 <div className={"flex flex-row justify-between  "}>
                     <div className={"flex gap-3 items-center"}>
-                        <p className={" text-lg text-dark-tertiary"}>29min</p>
+                        <p className={" text-lg text-dark-tertiary"}>{time}</p>
                         <img className={" h-4 w-4"} src={StarIcon} alt="Star"/>
-                        <p className={" text-lg text-dark-tertiary"}>4.8</p>
+                        <p className={" text-lg text-dark-tertiary"}>{rating}</p>
                     </div>
                     <div
                         className={"border  rounded-3xl bg-gray-secondary   w-8 h-8 flex  justify-center items-center"}>
                         <img src={BookMark} alt="Bookmark" className={"w-4 h-4"}/>
-
                     </div>
-
                 </div>
             </div>
         </div>
--- src/component/TopRestaurants/TopRestaurants.tsx
+++ src/component/TopRestaurants/TopRestaurants.tsx
@@ -1,4 +1,6 @@
 import RestaurantCard from "../RestaurantCard/RestaurantCard.tsx";
+import ChickenImage from "../../assets/ChickenImage.png"
+import BurgerImage from "../../assets/BurgerImage.png"
 
 const TopRestaurants = () => {
     return <>
@@ -8,10 +10,15 @@ const TopRestaurants = () => {
                     className={"text-purple-primary"}>Restaurants</span>
                 </p>
             </div>
-            <div className={"m-6 flex flex-col justify-center md:flex-row gap-4"}>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
+            <div className={"m-6 flex flex-col justify-center md:justify-between md:flex-row gap-4"}>
+                <RestaurantCard restaurantImage={ChickenImage} tagText={'Healthy'} time={'24min'}
+                                restaurantName={"The Chicken King"} rating={"4.8"}/>
+                <RestaurantCard
+                    restaurantImage={BurgerImage} tagText={'Trending'} time={'11min'}
+                    restaurantName={"The Burger King"} rating={"4.9"}/>
+                <RestaurantCard restaurantImage={ChickenImage} tagText={'Healthy'}
+                                time={'9min'} restaurantName={"The Noodles"} rating={"4.3"}/>
+
             </div>
         </div>
 
