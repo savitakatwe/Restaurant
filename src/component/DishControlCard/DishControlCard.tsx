191c988dd2d27e0c3ff72bfec5cc243189480a02
Savita Katwe <savitamerwade04@gmail.com>
2024-09-14 11:17:06 +0400

    card

--- src/component/DishControlCard/DishControlCard.tsx
+++ src/component/DishControlCard/DishControlCard.tsx
@@ -3,13 +3,17 @@ import FoodImage2 from "../../assets/FoodImage2.png";
 const DishControlCard = () => {
   return (
     <>
-      <div className={"flex flex-row "}>
+      <div
+        className={
+          "flex flex-row  gap-4 border  shadow-2xl rounded-2xl w-fit px-8 py-4"
+        }
+      >
         <img src={FoodImage2} alt={"FoodImage2"} className={"h-14 w-14"} />
-        <div>
-          <p className={"text-lg font-semibold"}>Chicken Hell</p>
+        <div className={"flex flex-col  "}>
+          <p className={"text-lg font-semibold "}>Chicken Hell</p>
           <p className={"text-sm text-dark-tertiary"}>On The Way</p>
         </div>
-        <p className={"text-sm text-gray-primary"}>3:06pm</p>
+        <p className={"text-sm text-gray-primary "}>3:06pm</p>
       </div>
     </>
   );
